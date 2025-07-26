3984c776fb6c1beb20c9f8d7800d4d7621b6e776
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-07-26 16:50:37 +0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -11,17 +11,17 @@ func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.POST("/register", controllers.Signup)
-    router.POST("/login", controllers.Login)
+        router.POST("/login", controllers.Login)
 	router.POST("/promote", middleware.AuthMiddleware(), middleware.AdminOnly(), controllers.PromoteUser) // Only admin
 	router.DELETE("/users/:id", middleware.AdminOnly(), controllers.DeleteUser)
-	router.GET("/users",   controllers.GetUsers)
+	router.GET("/users", middleware.AdminOnly(), controllers.GetUsers)
 
 	// Protected routes
 	protected := router.Group("/tasks")
 	protected.Use(middleware.AuthMiddleware()) // ✅ apply middleware
 	{
-		protected.GET("/", controllers.GetAllTasks) 
-		protected.GET("/:id", controllers.GetTask)
+	    protected.GET("/", controllers.GetAllTasks) 
+	    protected.GET("/:id", controllers.GetTask)
 	    protected.DELETE("/:id", middleware.AdminOnly(), controllers.DeleteTask)
 	    protected.POST("/", middleware.AdminOnly(), controllers.AddTask)
 	    protected.PUT("/:id", middleware.AdminOnly(), controllers.UpdateTask)
